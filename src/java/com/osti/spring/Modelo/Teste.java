3c629eaf273e45d8828a5a1c7ba02e69057a26b5
Samuel Mavungo <Samuel Mavungo@AvelinoMavungo>
2016-09-14 11:36:31 -0300

    Alterações feitas

---
+++ src/java/com/osti/spring/Modelo/Teste.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.osti.spring.Modelo;
+
+/**
+ *
+ * @author Samuel Mavungo
+ */
+public class Teste {
+    private String Tituilo;
+    private int Numero, Data;
+
+    public String getTituilo() {
+        return Tituilo;
+    }
+
+    public void setTituilo(String Tituilo) {
+        this.Tituilo = Tituilo;
+    }
+
+    public int getNumero() {
+        return Numero;
+    }
+
+    public void setNumero(int Numero) {
+        this.Numero = Numero;
+    }
+
+    public int getData() {
+        return Data;
+    }
+
+    public void setData(int Data) {
+        this.Data = Data;
+    }
+
+    
+}
