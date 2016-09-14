2b3bfca8727a382512c840c68727a79ce316ae7c
Samuel Mavungo <Samuel Mavungo@AvelinoMavungo>
2016-09-14 12:26:18 -0300

    Alterações2.2- Class teste2 numa nova classe.

--- src/java/com/osti/spring/Modelo/Teste.java
+++ src/java/com/osti/spring/Modelo/Teste.java
@@ -39,41 +39,5 @@ public class Teste {
     public void setData(Date data){
         this.data = data;
     }
-    public class Teste2{
-      private String nome, sexo;
-      private int idade;
-
-        public String getNome() {
-            return nome;
-        }
-
-        public void setNome(String nome) {
-            this.nome = nome;
-        }
-
-        public String getSexo() {
-            return sexo;
-        }
-
-        public void setSexo(String sexo) {
-            this.sexo = sexo;
-        }
-
-        public int getIdade() {
-            return idade;
-        }
-
-        public void setIdade(int idade) {
-            this.idade = idade;
-        }
-
-        public Date getNasc_mento() {
-            return nasc_mento;
-        }
-
-        public void setNasc_mento(Date nasc_mento) {
-            this.nasc_mento = nasc_mento;
-        }
-      private Date nasc_mento;
-    }
+   
 }
